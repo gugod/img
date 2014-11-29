f3397cdc710e1000ac4ca70a81de5e9555d2ab68
Kang-min Liu <gugod@gugod.org>
2014-11-29 11:18:54 +0100

    index/search with average hash

--- lib/Img/Analyzer.pm
+++ lib/Img/Analyzer.pm
@@ -4,41 +4,21 @@ package Img::Analyzer {
     use Imager;
     use List::MoreUtils 'uniq';
 
+    use Img::Hashing;
+
     sub analyze {
         my ($self, $file) = @_;
-        my $img = Imager->new( file => $file ) or die Imager->errstr;
-        $img = $img->convert( preset => "noalpha" )->scale( xpixels => 100, ypixels => 100, type => "nonprop", qtype => "preview" );
         my $doc = {};
-        for my $channel (0 .. $img->getchannels-1) {
-            my $img = $img->convert( preset => "channel${channel}" );
-            my $mat = [];
-            for my $y (0..$img->getheight-1) {
-                $mat->[$y] = [map { sprintf('%x', ($_->rgba)[0]) } $img->getscanline(y=>$y)];
-            }
-            my $feature = [];
-            my $feature3x3 = [];
-            for my $y (0..$img->getheight-3) {
-                for my $x (0..$img->getwidth-3) {
-                    push @$feature, $mat->[$y][$x];
-                    push @$feature3x3, join(
-                        "",
-                        $mat->[$y][$x],
-                        $mat->[$y][$x+1],
-                        $mat->[$y][$x+2],
-                        $mat->[$y+1][$x],
-                        $mat->[$y+1][$x+1],
-                        $mat->[$y+1][$x+2],
-                        $mat->[$y+2][$x],
-                        $mat->[$y+2][$x+1],
-                        $mat->[$y+2][$x+2],
-                    );
-                }
-            }
-            push @{$doc->{feature3x3}}, @$feature3x3;
-            push @{$doc->{feature}}, @$feature;
+        my $img = Imager->new( file => $file ) or die Imager->errstr;
+
+        my @avghash_tokens;
+        my $avghash = Img::Hashing::average_hash($img);
+        for my $i (0..63) {
+            my $b = ($avghash & (1<<$i)) >> $i;
+            push @avghash_tokens, $i . "_" . $b;
         }
-        $doc->{feature} = [uniq(@{$doc->{feature}})];
-        $doc->{feature3x3} = [uniq(@{$doc->{feature3x3}})];
+        $doc->{average_hash} = \@avghash_tokens;
+
         return $doc;
     }
 };
