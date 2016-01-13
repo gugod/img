a406c3faf1e34be65ad94599a4f756823d92f3a4
Kang-min Liu <gugod@gugod.org>
2016-01-13 23:25:53 +0100

    Improve speed.

--- bin/merge-overlapping-bboxes.pl
+++ bin/merge-overlapping-bboxes.pl
@@ -19,12 +19,15 @@ sub main {
     }
     close($fh);
 
+    my %seen;
     find_overlaps_and_merge(
         \@bbox, sub {
             my ($box) = @_;
             my $b = join(",", @$box);
-            say $b;
-            say $fh_out $b;
+            unless ($seen{$b}++) {
+                say $b;
+                say $fh_out $b;
+            }
         });
 
     close($fh_out);
@@ -34,43 +37,45 @@ main(@ARGV);
 sub find_overlaps_and_merge {
     my ($bbox, $cb) = @_;
 
-    for my $i (0..@$bbox-2) {
-        for my $j ($i+1..@$bbox-1) {
-            if (overlaps($bbox->[$i], $bbox->[$j])) {
-                $cb->( merge_bbox($bbox->[$i], $bbox->[$j]) );
+    my @points;
+    my %idx_p2b;
+    for my $i ( 0 ... @$bbox-1) {
+        my $bb = $bbox->[$i];
+        my @bb_points = (
+            [$bb->[2], $bb->[0], $i],
+            [$bb->[2], $bb->[1], $i],
+            [$bb->[3], $bb->[0], $i],
+            [$bb->[3], $bb->[1], $i],
+        );
+        push @points, @bb_points;
+        $bb->[5] = \@bb_points;
+    }
+
+    my @points_i_sorted_by_x = sort { $points[$a][0] <=> $points[$b][0] } (0...@points-1);
+    for my $i (0..@$bbox-1) {
+        my $bb = $bbox->[$i];
+        my @points_inside_bb;
+        for my $j (@points_i_sorted_by_x) {
+            my $p_x = $points[$j][0];
+            my $p_y = $points[$j][1];
+            if ( $bb->[2] <= $p_x && $p_x <= $bb->[3] ) {
+                if ( $bb->[0] <= $p_y && $p_y <= $bb->[1] ) {
+                    push @points_inside_bb, $points[$j];
+                }
             }
         }
+
+        my $merged_bb = merge_bboxes($bb, map { $bbox->[$_->[2]] } @points_inside_bb);
+        $cb->($merged_bb);
     }
 }
 
-sub point_in_box {
-    my ($point, $box) = @_;
-    return (
-        $box->[0] <= $point->[1] && $point->[1] <= $box->[1] &&
-        $box->[2] <= $point->[0] && $point->[0] <= $box->[3]
-    )
-}
-
-sub overlaps {
-    my ($box1, $box2) = @_;
-    my @p = (
-        [$box2->[2], $box2->[0]],
-        [$box2->[2], $box2->[1]],
-        [$box2->[3], $box2->[0]],
-        [$box2->[3], $box2->[1]],
-    );
-    for my $p (@p) {
-        return 1 if point_in_box($p, $box1);
-    }
-    return 0;
-}
-
-sub merge_bbox {
-    my ($box1, $box2) = @_;
+sub merge_bboxes {
+    my @boxes = @_;
     return [
-        min($box1->[0], $box2->[0]),
-        max($box1->[1], $box2->[1]),
-        min($box1->[2], $box2->[2]),
-        max($box1->[3], $box2->[3]),
+        min(map { $_->[0] } @boxes),
+        max(map { $_->[1] } @boxes),
+        min(map { $_->[2] } @boxes),
+        max(map { $_->[3] } @boxes),
     ]
 }
